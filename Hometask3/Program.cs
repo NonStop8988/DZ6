875b78f96ca98296fdda3631ecd8c130f0bbb166
Maxim <lyskomax2@mail.ru>
2024-01-26 17:50:21 +0300

    Add Hometask3

---
+++ Hometask3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+// Функция - проверка строки на палиндромность
+void PrintPalindromeString(string str)
+{
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - 1 - i])
+        {
+            Console.Write("Введенная строка НЕ является палиндромом");
+            break;
+        }
+        else if (str[i] == str[str.Length - 1 - i] && i == str.Length / 2 - 1)
+        {
+            Console.Write("Введенная строка ЯВЛЯЕТСЯ палиндромом");
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите строку, чтобы проверить, является ли она палиндромом: ");
+string str = Console.ReadLine()!;
+PrintPalindromeString(str);
