7264d202232c579b4f8304f4a2f912ec0550f312
Maxim <lyskomax2@mail.ru>
2024-01-26 17:21:12 +0300

    Add Hometask2

---
+++ Hometask2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+Console.Clear();
+string originalString  = "aBcD1ef!-";
+Console.WriteLine($"Изначальная строка: {originalString}");
+string lowerCaseString  = originalString.ToLower();
+Console.WriteLine($"Строка с измененным регистром символа: {lowerCaseString}");
